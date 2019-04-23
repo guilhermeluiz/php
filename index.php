72e9336b3675fc1cd653d893a2249592df5e200f
Guilherme Luiz <guilherme.10luiz@gmail.com>
2019-04-23 12:40:04 -0300

    ajeitei o codigo em conflito

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
 
-<<<<<<< HEAD
+
 echo "olá mundo! 456";
 
-echo "hello darkness";
-=======
-echo "olá mundo! 1 2 3";
-
-echo "olá aluno";
->>>>>>> c0cf47173ee132756694e9f2acf64b5b91575eca
+echo "hello darkness and aluno";
 
 ?>
